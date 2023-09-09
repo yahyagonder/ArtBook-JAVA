c7d92180b473d85180a34d4239c5e250a3acb0bd
yahyagonder <yahyagonder@outlook.com>
2023-09-10 02:40:31 +0300

    initial create

---
+++ app/src/main/java/com/yahyagonder/artbookjava/Art.java
@@ -0,0 +1,14 @@
+package com.yahyagonder.artbookjava;
+
+public class Art {
+
+    String name;
+
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+}
